e0ed889ba8fdbcda7dbe7ef9025bd261521b7b65
Dunvael Le Roux <simplon-lerouxDunvael@gmail.com>
2023-09-04 15:52:19 +0200

    updated

--- main.tf
+++ main.tf
@@ -9,6 +9,28 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
+# Déploiement du cluster AKS
+resource "azurerm_kubernetes_cluster" "AKS" {
+  name                = var.aks_name
+  location            = module.network.location
+  resource_group_name = module.network.resource_group_name
+
+  default_node_pool {
+    name       = var.node_pool_name
+    node_count = var.node_count
+    vm_size    = var.vm_size
+    vnet_subnet_id = module.network.subnet_id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = var.tags
+  }
+}
+
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   resource_group_name = azurerm_resource_group.rg.name
@@ -25,10 +47,22 @@ resource "azurerm_subnet" "subnet" {
 
 # ... ajouter NAT Gateway et Route Table ici comme bonus
 
+
+
+# Outputs
+
 output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
 output "subnet_id" {
   value = azurerm_subnet.subnet.id
+}
+
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.AKS.kube_config_raw
 }
