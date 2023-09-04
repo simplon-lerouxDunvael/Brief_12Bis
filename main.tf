121b89d32c5f815c4f9e1cc7e000064a5e7d437e
Dunvael Le Roux <simplon-lerouxDunvael@gmail.com>
2023-09-04 11:49:05 +0200

    main finished (for part one)

--- main.tf
+++ main.tf
@@ -4,31 +4,31 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = var.resource_group_name
-  location = var.location
+resource "azurerm_resource_group" "rg" {
+  name     = local.resource_group_name[rg1]
+  location = local.location[france]
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = var.vnet_name
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  address_space       = [var.address_space]
+resource "azurerm_virtual_network" "vnet" {
+  name                = local.vnet_name[vnet1]
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = local.address_space["vnet1add"]
 }
 
-resource "azurerm_subnet" "example" {
-  name                 = var.subnet_name
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = [var.subnet_prefix]
+resource "azurerm_subnet" "subnet" {
+  name                 = local.subnet_name[sbnt1]
+  resource_group_name  = azurerm_resource_group.vnet.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = local.subnet_prefix["sbnt-pref2"] # pour mettre plusieurs clés de variables : [local.subnet_prefix["sbnt-pref2"], ["autre clé"]]
 }
 
 # ... ajouter NAT Gateway et Route Table ici comme bonus
 
 output "resource_group_name" {
-  value = azurerm_resource_group.example.name
+  value = azurerm_resource_group.rg.name
 }
 
 output "subnet_id" {
-  value = azurerm_subnet.example.id
+  value = azurerm_subnet.subnet.id
 }
