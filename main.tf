b3912289bd501bc396cee9e7fcaa0735284b397b
Dunvael Le Roux <simplon-lerouxDunvael@gmail.com>
2023-09-04 15:59:45 +0200

    aks updated

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ output "subnet_id" {
 }
 
 output "client_certificate" {
-  value = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+  value = azurerm_kubernetes_cluster.AKS.kube_config.0.client_certificate
 }
 
 output "kube_config" {
