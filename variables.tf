d65c56aa28f6d88e543e8d0197d27e93b6cb927a
Dunvael Le Roux <simplon-lerouxDunvael@gmail.com>
2023-09-04 11:44:02 +0200

    vars updated with proper config

--- variables.tf
+++ variables.tf
@@ -1,34 +1,34 @@
 # Variables pour le déploiement du module network
 locals {
 
-variable "resource_group_name" {
-  rg1 = "db12-rg"
-  rg2 = "db13-rg"
+resource_group_name = {
+  "rg1" = "db12-rg"
+  "rg2" = "db13-rg"
 }
 
-variable "location" {
-  france = "francecentral"
-  europe = "westeurope"
+location = {
+  "france" = "francecentral"
+  "europe" = "westeurope"
 }
 
-variable "vnet_name" {
-  vnet1 = "db12-vnet"
-  vnet2 = "db13-vnet"
+vnet_name = {
+  "vnet1" = "db12-vnet"
+  "vnet2" = "db13-vnet"
 }
 
-variable "address_space" {
-  vnet1add = "10.0.1.0/24"
-  vnet2add = "10.0.2.0/24"
+address_space = {
+  "vnet1add" = "10.0.1.0/24"
+  "vnet2add" = "10.0.2.0/24"
 }
 
-variable "subnet_name" {
-  sbnt1 = "db12-sbnt"
-  sbnt2 = "db13-sbnt"
+subnet_name = {
+  "sbnt1" = "db12-sbnt"
+  "sbnt2" = "db13-sbnt"
 }
 
-variable "subnet_prefix" {
-  sbnt-pref1 = "10.0.0.0/16"
-  sbnt-pref2 = "10.0.0.0/16"
+subnet_prefix = {
+  "sbnt-pref1" = "10.0.0.0/16"
+  "sbnt-pref2" = "10.0.0.0/16"
 }
 
 }
