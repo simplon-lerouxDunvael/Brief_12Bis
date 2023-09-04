bdf8f6d37ec09b6234424794b403cd1e69afca53
Dunvael Le Roux <simplon-lerouxDunvael@gmail.com>
2023-09-04 11:10:16 +0200

    var completed

--- variables.tf
+++ variables.tf
@@ -1,25 +1,37 @@
 # Variables pour le déploiement du module network
 
 variable "resource_group_name" {
+  description = "Nom du groupe de ressources"
   type = string
+  default = "db12-rg"
 }
 
 variable "location" {
+  description = "Localisation des ressources"
   type = string
+  default = "francecentral"
 }
 
 variable "vnet_name" {
+  description = "Nom du Vnet"
   type = string
+  default = "db12-vnet"
 }
 
 variable "address_space" {
+  description = "Adresse IP"
   type = string
+  default = "10.0.1.0/24"
 }
 
 variable "subnet_name" {
+  description = "¨Nom du sous-réseau"
   type = string
+  default = "db12-sbnt"
 }
 
 variable "subnet_prefix" {
+  description = "Masque de sous-réseau"
   type = string
+  default = "10.0.0.0/16"
 }
