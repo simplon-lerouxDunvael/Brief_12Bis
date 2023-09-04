a46666a8bdad23f049527ebd30376e675f70b8fa
Dunvael Le Roux <simplon-lerouxDunvael@gmail.com>
2023-09-04 14:38:36 +0200

    variables ajouétes et définies

--- variables.tf
+++ variables.tf
@@ -1,37 +1,73 @@
-# Variables pour le déploiement du module network
+# Variables pour le déploiement du module avec les ressources et le cluster AKS
+
+variable aks_name {
+  description = "Nom du cluster AKS"
+  type = string
+  aks = "db12-AKS"
+}
 
 variable resource_group_name {
-  description = "value"
+  description = "Nom du RG"
   type = string
   rg1 = "db12-rg"
 }
 
 variable location {
-  description = "value"
+  description = "Localisation des ressources"
   type = string
   default = "francecentral"
 }
 
 variable vnet_name {
-  description = "value"
+  description = "Nom du Vnet"
   type = string
   default = "db12-vnet"
 }
 
 variable address_space {
-  description = "value"
+  description = "CIDR"
   type = string
   default = "10.0.1.0/24"
 }
 
 variable subnet_name {
-  description = "value"
+  description = "Nom du subnet"
   type = string
   default = "db12-sbnt"
 }
 
 variable subnet_prefix {  
-  description = "value"
+  description = "Plage d'adresses IP pour le sous-réseau"
   type = string
   default = "10.0.0.0/16"
+}
+
+node_pool_name = {
+  description = "Nom du node pool"
+  type = string
+  default = "pool-db12"
+}
+
+node_count = {
+  description = "Nombre de nodes"
+  type = number
+  default = "2"
+}
+
+vm_size = {
+  description = "Taille de la VM"
+  type = string
+  default = "Standard_D2_v2"
+}
+
+identity = {
+  description = "Identité"
+  type = string
+  default = "SystemAssigned"
+}
+
+tags = {
+  description = "Tag"
+  type = string
+  default = "dev"
 }
