dd8560eea4c2478dd31c8eeb014798f07a705346
Dunvael Le Roux <simplon-lerouxDunvael@gmail.com>
2023-09-04 14:22:52 +0200

    var file ok

--- variables.tf
+++ variables.tf
@@ -1,34 +1,37 @@
 # Variables pour le déploiement du module network
-locals {
 
-resource_group_name = {
-  "rg1" = "db12-rg"
-  "rg2" = "db13-rg"
+variable resource_group_name {
+  description = "value"
+  type = string
+  rg1 = "db12-rg"
 }
 
-location = {
-  "france" = "francecentral"
-  "europe" = "westeurope"
+variable location {
+  description = "value"
+  type = string
+  default = "francecentral"
 }
 
-vnet_name = {
-  "vnet1" = "db12-vnet"
-  "vnet2" = "db13-vnet"
+variable vnet_name {
+  description = "value"
+  type = string
+  default = "db12-vnet"
 }
 
-address_space = {
-  "vnet1add" = "10.0.1.0/24"
-  "vnet2add" = "10.0.2.0/24"
+variable address_space {
+  description = "value"
+  type = string
+  default = "10.0.1.0/24"
 }
 
-subnet_name = {
-  "sbnt1" = "db12-sbnt"
-  "sbnt2" = "db13-sbnt"
+variable subnet_name {
+  description = "value"
+  type = string
+  default = "db12-sbnt"
 }
 
-subnet_prefix = {
-  "sbnt-pref1" = "10.0.0.0/16"
-  "sbnt-pref2" = "10.0.0.0/16"
-}
-
+variable subnet_prefix {  
+  description = "value"
+  type = string
+  default = "10.0.0.0/16"
 }
