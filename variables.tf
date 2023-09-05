2f68b111250edce53bb7138f35961be9028b5924
Dunvael Le Roux <simplon-lerouxDunvael@gmail.com>
2023-09-05 14:37:15 +0200

    cidr updated

--- variables.tf
+++ variables.tf
@@ -37,7 +37,7 @@ variable subnet1_name {
 variable subnet1_prefix {  
   description = "Plage d'adresses IP pour le sous-réseau 1"
   type = list(string)
-  default = ["10.0.1.0/16"]
+  default = ["10.0.1.0/24"]
 }
 
 variable gateway_name {  
@@ -85,7 +85,7 @@ variable priv_subnet_name {
 variable priv_sbnt_add_pref {  
   description = "Plage d'adresses IP pour le sous-réseau privé"
   type = list(string)
-  default = ["10.0.2.0/16"]
+  default = ["10.0.2.0/24"]
 }
 
 variable pub_subnet_name {  
@@ -97,7 +97,7 @@ variable pub_subnet_name {
 variable pub_sbnt_add_pref {  
   description = "Plage d'adresses IP pour le sous-réseau publique"
   type = list(string)
-  default = ["10.0.3.0/16"]
+  default = ["10.0.3.0/24"]
 }
 
 # Variables pour l'AKS
