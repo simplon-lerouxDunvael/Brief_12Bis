7004eb7a453869a77101e79881fc7569b49aabde
Dunvael Le Roux <simplon-lerouxDunvael@gmail.com>
2023-09-05 14:20:48 +0200

    dns added

--- main.tf
+++ main.tf
@@ -14,7 +14,8 @@ resource "azurerm_kubernetes_cluster" "AKS" {
   name                = var.aks_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-
+  dns_prefix          = var.dns_prefix
+  
   default_node_pool {
     name       = var.node_pool_name
     node_count = var.node_count
