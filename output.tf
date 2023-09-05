60d324dd56ab222f7338e41e273ce1ff13c48d28
Dunvael Le Roux <simplon-lerouxDunvael@gmail.com>
2023-09-05 11:02:35 +0200

    outputs added

--- output.tf
+++ output.tf
@@ -3,13 +3,21 @@ output "resource_group_name" {
 }
 
 output "subnet_id" {
-  value = azurerm_subnet.subnet.id
+  value = azurerm_subnet.subnet1.id
 }
 
 output "client_certificate" {
-  value = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+  value = azurerm_kubernetes_cluster.AKS.kube_config.0.client_certificate
 }
 
 output "kube_config" {
   value = azurerm_kubernetes_cluster.AKS.kube_config_raw
+}
+
+output "priv_subnet_id" {
+  value = azurerm_subnet.priv_subnet.id
+}
+
+output "pub_subnet_id" {
+  value = azurerm_subnet.pub_subnet.id
 }
